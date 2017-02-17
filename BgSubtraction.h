adf3f974fcc514ea133bf53059d9022f14659f47
Nikhil Jagdale <njagdale@gmail.com>
2017-02-16 16:29:06 -0800

    BgSubtraction.* ==> Demo's motion tracking of player and shuttle HeatMap ==> Generate's a rough motion based heatmap that needs improvement

---
+++ BgSubtraction.h
@@ -0,0 +1,10 @@
+//opencv
+#include "opencv2/imgcodecs.hpp"
+#include "opencv2/imgproc.hpp"
+#include "opencv2/videoio.hpp"
+#include <opencv2/highgui.hpp>
+#include <opencv2/video.hpp>
+#include <iostream>
+#include <sstream>
+
+int bgSub();
